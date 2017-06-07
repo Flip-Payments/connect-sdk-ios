4d99b033beb0488de1d79f954d602201f5fb5137
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-07 00:07:21 -0300

    :heavy_plus_sign: Adds Constants

---
+++ FlipConnectSDK/FCConsts.swift
@@ -0,0 +1,14 @@
+//
+//  FCConsts.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 07/06/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import Foundation
+
+struct FCConsts {
+    static let connectWebUrl = "http://flipconnect-signin-staging.herokuapp.com/"
+    static let connectApiUrl = "http://dlp-qrservices.cloudapp.net:20112/"
+}
