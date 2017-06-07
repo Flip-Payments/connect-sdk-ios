f90265530212107699453bc30d25b4caac676ae8
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-06 21:40:33 -0300

    Initial Commit

---
+++ FlipConnectSDK/FlipConnectSDK.h
@@ -0,0 +1,19 @@
+//
+//  FlipConnectSDK.h
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 06/06/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlipConnectSDK.
+FOUNDATION_EXPORT double FlipConnectSDKVersionNumber;
+
+//! Project version string for FlipConnectSDK.
+FOUNDATION_EXPORT const unsigned char FlipConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlipConnectSDK/PublicHeader.h>
+
+
