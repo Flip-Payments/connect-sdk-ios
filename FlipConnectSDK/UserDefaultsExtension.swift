fb814b6e8f11ef107417b177b83c987c6484ad8b
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-06 21:46:30 -0300

    :heavy_plus_sign: Adds UserDefaultsExtension

---
+++ FlipConnectSDK/UserDefaultsExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefaultsExtension.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 06/06/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+    private struct Keys {
+        static let AuthorizationCode = "FCAuthorizationCode"
+    }
+    
+    public internal(set) var authorizationCode: String? {
+        get {
+            return UserDefaults.standard.string(forKey: Keys.AuthorizationCode)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: Keys.AuthorizationCode)
+        }
+    }
+}
