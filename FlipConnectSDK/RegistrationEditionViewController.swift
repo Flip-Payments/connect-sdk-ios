4e2516444fd411126e58c176e7b262c2205ccc0c
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-26 15:46:40 -0300

    🚧  Add Storyboard and associated controller

---
+++ FlipConnectSDK/RegistrationEditionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegistrationEditionViewController.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 23/06/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationEditionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
