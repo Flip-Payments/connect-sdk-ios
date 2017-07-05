62a9632ca045c2320cbeddce7d30589697921008
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-05 16:57:49 -0300

    ➕  Add User to entity

--- FlipConnectSDK/User.swift
+++ FlipConnectSDK/User.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct User {
+    var acountKey: String
+    var isEnabled: Bool
+    var membershipCreateDate: Date
+    var isNewsLetterllowed: Bool
+    var publicProfile: PublicProfile?
+    var emails: [Email]
+    var telephones: [Telephone]
+}
