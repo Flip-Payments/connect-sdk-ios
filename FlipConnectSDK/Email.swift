9d1e195772a2793ecc3f863a838eb6979b1cbc44
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-05 16:56:20 -0300

    🚧  Change email entity to accept keys

--- FlipConnectSDK/Email.swift
+++ FlipConnectSDK/Email.swift
@@ -9,17 +9,23 @@
 import Foundation
 
 struct Email {
-    var id: Int
+    var id: String
     var email: String
+    var isValidated: Bool
+    var isPrimary: Bool
     
     init?(json: JSON) {
-        guard let id = json["id"] as? Int,
-            let email = json["email"] as? String
+        guard let id = (json["id"] as? String ?? json["key"] as? String),
+            let email = json["email"] as? String,
+            let isValidated = json["isValidated"] as? Bool,
+            let isPrimary = json["isPrimary"] as? Bool
             else {
                 return nil
         }
         
         self.id = id
         self.email = email
+        self.isValidated = isValidated
+        self.isPrimary = isPrimary
     }
 }
---
+++ FlipConnectSDK/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 05/07/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import Foundation
