1a8215690d352a756db5b6036d032acb412f18ce
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-05 19:39:11 -0300

    🚧  Final touches to user entity

--- FlipConnectSDK/Email.swift
+++ FlipConnectSDK/Email.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct Email {
     var id: String
-    var email: String
+    var address: String
     var isValidated: Bool
     var isPrimary: Bool
     
     init?(json: JSON) {
         guard let id = (json["id"] as? String ?? json["key"] as? String),
-            let email = json["email"] as? String,
+            let address = json["address"] as? String,
             let isValidated = json["isValidated"] as? Bool,
             let isPrimary = json["isPrimary"] as? Bool
             else {
@@ -24,7 +24,7 @@ struct Email {
         }
         
         self.id = id
-        self.email = email
+        self.address = address
         self.isValidated = isValidated
         self.isPrimary = isPrimary
     }
--- FlipConnectSDK/User.swift
+++ FlipConnectSDK/User.swift
@@ -9,11 +9,42 @@
 import Foundation
 
 struct User {
-    var acountKey: String
+    var accountKey: String
     var isEnabled: Bool
     var membershipCreateDate: Date
-    var isNewsLetterllowed: Bool
+    var isNewsLetterAllowed: Bool
     var publicProfile: PublicProfile?
-    var emails: [Email]
-    var phones: [Phone]
+    var emails: [Email] = []
+    var phones: [Phone] = []
+    
+    init?(json: JSON) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M/DD/YYYY HH:mm:ss a"
+        
+        guard let accountKey = json["accountKey"] as? String,
+            let isEnabled = json["isEnabled"] as? Bool,
+            let membershipCreateDateString = json["membershipCreateDate"] as? String,
+            let membershipCreateDate = dateFormatter.date(from: membershipCreateDateString),
+            let isNewsLetterAllowed = json["isNewsLetterAllowed"] as? Bool
+            else {
+                return nil
+        }
+        
+        self.accountKey = accountKey
+        self.isEnabled = isEnabled
+        self.membershipCreateDate = membershipCreateDate
+        self.isNewsLetterAllowed = isNewsLetterAllowed
+        self.publicProfile = nil
+        if let publicProfileJSON = json["publicProfile"] as? JSON {
+            self.publicProfile = PublicProfile(json: publicProfileJSON)
+        }
+        if let emailsJSON = json["emails"] as? [JSON] {
+            let emails = emailsJSON.flatMap{ Email(json: $0) }
+            self.emails = emails
+        }
+        if let phonesJSON = json["phones"] as? [JSON] {
+            let phones = phonesJSON.flatMap{ Phone(json: $0) }
+            self.phones = phones
+        }
+    }
 }
