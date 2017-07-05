99f000f7e5d34f42e8cf23c072949e2c781b3eab
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-05 19:38:51 -0300

    ✅  Add User test

--- FlipConnectSDKTests/EntitiesTests.swift
+++ FlipConnectSDKTests/EntitiesTests.swift
@@ -100,6 +100,17 @@ class EntitiesTests: XCTestCase {
         
         XCTAssert(telephone?.type == .home)
     }
+    
+    func testJSONtoUserEntityConversion() {
+        let userJSON = "{\"accountKey\":\"5e2e42ce-0985-4d0c-88d3-0a02aed397b1\",\"isEnabled\":true,\"membershipCreateDate\":\"6/22/2017 10:04:37 PM\",\"isNewsLetterAllowed\":true,\"publicProfile\":{\"name\":\"Cesar Maia\",\"pictureUrl\":null},\"personalData\":null,\"emails\":[{\"key\":\"d16fc241-2f5c-e711-80c2-0003ff345086\",\"address\":\"h797176@mvrht.net\",\"isValidated\":true,\"isPrimary\":true}],\"phones\":[{\"key\":\"8d6dc241-2f5c-e711-80c2-0003ff345086\",\"phoneType\":\"mobile\",\"phoneTypeFriendlyName\":\"Celular\",\"fullNumber\":\"+5521987654321\",\"isValidated\":false,\"isPrimary\":false},{\"key\":\"8e6dc241-2f5c-e711-80c2-0003ff345086\",\"phoneType\":\"home\",\"phoneTypeFriendlyName\":\"Residencial\",\"fullNumber\":\"+5521123456789\",\"isValidated\":false,\"isPrimary\":false}]}"
+        
+        let data = userJSON.data(using: String.Encoding.utf8)!
+        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! JSON
+        
+        let user = User(json: json)
+        
+        XCTAssert(user?.accountKey == "5e2e42ce-0985-4d0c-88d3-0a02aed397b1")
+    }
 
     func testAPICallToCollectAddresses() {
         let requestExpectation = expectation(description: "Get addresses collection")
