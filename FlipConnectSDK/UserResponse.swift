1217a71b02878fe37700e369039611a7cc361964
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-07 11:30:12 -0300

    ✅  Add UserResponse Test

--- FlipConnectSDK/UserResponse.swift
+++ FlipConnectSDK/UserResponse.swift
@@ -25,23 +25,3 @@ struct UserResponse: Response {
         self.user = User(json: user)
     }
 }
-
-struct UsersResponse: Response {
-    var operationReport: [Report] = []
-    
-    var success: Bool = false
-    
-    var users: [User] = []
-    
-    init(json: JSON) {
-        self.initializeResponse(json: json)
-        
-        guard let users = json["users"] as? [JSON] else {
-            return
-        }
-        
-        let user = users.flatMap{ User(json: $0) }
-        
-        self.users = user
-    }
-}
--- FlipConnectSDKTests/EntitiesTests.swift
+++ FlipConnectSDKTests/EntitiesTests.swift
@@ -111,6 +111,18 @@ class EntitiesTests: XCTestCase {
         
         XCTAssert(user?.accountKey == "5e2e42ce-0985-4d0c-88d3-0a02aed397b1")
     }
+    
+    func testJSONtoUserResponseConversion() {
+        let userJSON = "{\"user\":{\"accountKey\":\"5e2e42ce-0985-4d0c-88d3-0a02aed397b1\",\"isEnabled\":true,\"membershipCreateDate\":\"6/22/2017 10:04:37 PM\",\"isNewsLetterAllowed\":true,\"publicProfile\":{\"name\":\"Cesar Maia\",\"pictureUrl\":null},\"personalData\":null,\"emails\":[{\"key\":\"d16fc241-2f5c-e711-80c2-0003ff345086\",\"address\":\"h797176@mvrht.net\",\"isValidated\":true,\"isPrimary\":true}],\"phones\":[{\"key\":\"8d6dc241-2f5c-e711-80c2-0003ff345086\",\"phoneType\":\"mobile\",\"phoneTypeFriendlyName\":\"Celular\",\"fullNumber\":\"+5521987654321\",\"isValidated\":false,\"isPrimary\":false},{\"key\":\"8e6dc241-2f5c-e711-80c2-0003ff345086\",\"phoneType\":\"home\",\"phoneTypeFriendlyName\":\"Residencial\",\"fullNumber\":\"+5521123456789\",\"isValidated\":false,\"isPrimary\":false}]},\"success\":true,\"operationReport\":[]}"
+        
+        let data = userJSON.data(using: String.Encoding.utf8)!
+        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! JSON
+        
+        let user = UserResponse(json: json)
+        
+        XCTAssert(user.user?.accountKey == "5e2e42ce-0985-4d0c-88d3-0a02aed397b1")
+        XCTAssertTrue(user.success)
+    }
 
     func testAPICallToCollectAddresses() {
         let requestExpectation = expectation(description: "Get addresses collection")
