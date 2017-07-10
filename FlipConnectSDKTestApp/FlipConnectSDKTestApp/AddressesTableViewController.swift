3ffbe74723e9bd07a9b125c4b03048dff9b3ad82
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-10 20:51:01 -0300

    ➕ Add Addresses ViewTable Mock

--- FlipConnectSDKTestApp/FlipConnectSDKTestApp/AddressesTableViewController.swift
+++ FlipConnectSDKTestApp/FlipConnectSDKTestApp/AddressesTableViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class AddressesTableViewController: UITableViewController {
+class AddressesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
 
+    @IBAction func dismiss(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 200.0
+
+        tableView.register(cellNib: AddressTableViewCell.self)
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -20,32 +33,41 @@ class AddressesTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "addressCell") as! AddressTableViewCell
+        let cell: AddressTableViewCell = tableView.dequeueReusableCell(for: indexPath)
+        
         // Configure the cell...
+//        cell.cityStateLbl.text = "Rio de Janeiro-RJ"
+//        cell.complementLbl.text = "Apto 303"
+//        cell.countryFlagLbl.text = "🇧🇷"
+//        cell.districtLbl.text = "Tijuca"
+//        cell.streetLbl.text = "Rua Conde de Bonfim, 803"
+//        cell.zipCodeLbl.text = "20270-101"
+        
 
         return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    private func tableView(_ tableView: UITableView, willDisplay cell: AddressTableViewCell, forRowAt indexPath: IndexPath) {
+        cell.cityStateLbl.text = "Rio de Janeiro-RJ"
+        cell.complementLbl.text = "Apto 303"
+        cell.countryFlagLbl.text = "🇧🇷"
+        cell.districtLbl.text = "Tijuca"
+        cell.streetLbl.text = "Rua Conde de Bonfim, 803"
+        cell.zipCodeLbl.text = "20270-101"
+    }
 
     /*
     // Override to support conditional editing of the table view.
