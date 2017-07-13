e42228040e79247a7e2b13b615bcbef64e28a2de
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-13 11:35:58 -0300

    🚧  Add PublicProfileCell

---
+++ FlipConnectSDK/PublicProfileCell.swift
@@ -0,0 +1,28 @@
+//
+//  PublicProfileCell.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 13/07/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import UIKit
+
+class PublicProfileCell: UITableViewCell {
+
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
