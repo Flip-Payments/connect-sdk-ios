555f6ab333a3937f9e63275998875fb6273e6cb0
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-13 13:13:51 -0300

    🚧  Adds PersonalDataCell

---
+++ FlipConnectSDK/PersonalDataCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonalDataCell.swift
+//  FlipConnectSDK
+//
+//  Created by Munir Wanis on 13/07/17.
+//  Copyright © 2017 Flip Connect. All rights reserved.
+//
+
+import UIKit
+
+class PersonalDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
