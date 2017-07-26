595ff3b9f2d358c636e026881b14e82f5f792f66
Munir Wanis <fallen.angel.pp@gmail.com>
2017-07-26 10:46:37 -0300

    🎨  Creates Swift file exclusive to BasePatchRequest

--- FlipConnectSDK/PatchRequestProtocol.swift
+++ FlipConnectSDK/PatchRequestProtocol.swift
@@ -13,24 +13,3 @@ protocol PatchRequestProtocol {
     
     func add(operation: Operation, path: EnumType, value: String?)
 }
-
-class BasePatchRequest {
-    var patches: [Patch]? = nil
-    
-    var key: String? = nil
-    
-    func createDictionary() -> JSON {
-        if let patches = patches {
-            var patchesDicts: [JSON] = []
-            
-            for patch in patches {
-                patchesDicts.append(patch.createDictionary())
-            }
-            return [
-                "key": key as Any,
-                "patches": patchesDicts
-            ]
-        }
-        return [:]
-    }
-}
